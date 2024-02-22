745b5fead5fce39aefe12ab848752df31a30f319
Abdulsamad <lasisiabdulsamad9@gmail.com>
2024-02-22 21:56:27 +0100

    adjustment on the number of questions taking

--- Game.java
+++ Game.java
@@ -5,22 +5,29 @@ public class Game {
 
         Scanner scanner = new Scanner(System.in);
 
+
+        System.out.println("CHILDREN MATH GAME");
+
         int scores = 0;
 
-        for (int i = 0; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             long Num1 = Math.round((Math.random() * 10) + 1 );
             long Num2 = Math.round((Math.random() * 10) + 1 );
 
             long correctAnswer = Num1 + Num2;
-            System.out.print("What is the sum of " + Num1 + " and " + Num2 + " : ");
+            System.out.print("\nWhat is the sum of " + Num1 + " and " + Num2 + " : ");
             int answer = scanner.nextInt();
 
             if (answer == correctAnswer) {
                 scores++;
+                System.out.println("You're Correct");
+            }
+            else{
+                System.out.println("You're Wrong");
             }
         }
 
-        System.out.println("Your score is : " + scores + "/10");
+        System.out.println("\nYour score is : " + scores + "/10");
         if (scores >= 0 && scores <= 3) {
             System.out.println("This is very poor performance buckle up next time");
         }
